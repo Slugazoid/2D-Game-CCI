be9884249e32ebcc3d5bb2248944dd3a885ff40f
Reksa Aditya <reksaparamaaditya@gmail.com>
2026-08-14 15:58:53 +0800

    fix: wall gap and add audio

--- Objects/wall-kanan.gd
+++ Objects/wall-kanan.gd
@@ -5,11 +5,11 @@ extends Sprite2D
 @export var end_point: Vector2 = Vector2(1280, 200)
 
 @export var speed: float = 0.7
-@export var min_scale: Vector2 = Vector2(0.1, 0.1)
+@export var min_scale: Vector2 = Vector2(0.2, 0.2)
 @export var max_scale: Vector2 = Vector2(4.0, 4.0)
 
 @export var min_z_index: int = 0
-@export var max_z_index: int = 100
+@export var max_z_index: int = 1
 
 @export var initial_progress: float = 0.0
 var progress: float = 0.0
