88ed70b48c77f71388749c38f04fc50897642062
Reksa Aditya <reksaparamaaditya@gmail.com>
2026-08-20 09:12:19 +0800

    fix: clear script bug

--- Objects/clear.gd
+++ Objects/clear.gd
@@ -7,7 +7,6 @@ var clear_time: float = 0.0
 
 func _ready() -> void:
 	_load_clear_time()
-	_display_time()
 
 func _load_clear_time() -> void:
 	if SceneTransition and SceneTransition.pending_data.has("clear_time"):
@@ -17,10 +16,6 @@ func _load_clear_time() -> void:
 		SceneTransition.pending_data.erase("clear_time")
 	else:
 		push_warning("ClearScene: clear_time gak ditemukan di SceneTransition.pending_data, default ke 0.0")
-
-func _display_time() -> void:
-	if time_label:
-		time_label.text = "Waktu: %s" % GameplayManager.format_time(clear_time)
 
 ## Contoh handler kalau kamu tambah tombol "Main Menu" di scene ini
 func _on_main_menu_button_pressed() -> void:
